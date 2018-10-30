17b7da1c931cce458b8ec1f8d1ee67f5327947d6
Gzx <1003181892@qq.com>
2018-10-30 22:12:24 +0800

    commit

--- src/main/java/com/example/SpringDemoApplication.java
+++ src/main/java/com/example/SpringDemoApplication.java
@@ -2,11 +2,19 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringDemoApplication {
+public class SpringDemoApplication extends SpringBootServletInitializer {
 
-    public static void main(String[] args) {
+    @Override
+    protected SpringApplicationBuilder configure(final SpringApplicationBuilder application) {
+        return application.sources(SpringDemoApplication.class);
+    }
+
+    public static void main(String[] args) throws Exception {
         SpringApplication.run(SpringDemoApplication.class, args);
     }
+
 }
